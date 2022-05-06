d0a2836956f7add500b719fd5eb4508676afa02f
Barış Deniz Sağlam <bdsaglam@gmail.com>
2022-05-07 01:58:26 +0300

    refactor evaluation for mtl

--- vqa/evaluation.py
+++ vqa/evaluation.py
@@ -18,20 +18,18 @@ warnings.filterwarnings(action='ignore', category=UndefinedMetricWarning, module
 
 def evaluate_mtl(vocabs, probs, targets, preds, show=False):
     for vocab, target, pred in zip(vocabs, targets, preds):
+        target = target.cpu().numpy()
+        pred = pred.cpu().numpy()
         label_indices = list(range(len(vocab)))
-        y_true, y_pred = target.cpu().numpy(), pred.cpu().numpy()
-        clf_report = classification_report(y_true, y_pred, labels=label_indices, target_names=vocab)
+        clf_report = classification_report(target, pred, labels=label_indices, target_names=vocab)
         if show:
             print(clf_report)
             fig, ax = plt.subplots(figsize=(16, 12))
-            ConfusionMatrixDisplay.from_predictions(y_true, y_pred, labels=label_indices, display_labels=vocab, ax=ax)
-
-    distortion_f1_macro = F1Score(average='macro')(preds[0], targets[0])
-    distortion_accuracy = accuracy(probs[0], targets[0])
-    severity_accuracy = accuracy(probs[1], targets[1])
-    return dict(
-        classification_report=clf_report,
-        distortion_f1_macro=distortion_f1_macro,
-        distortion_accuracy=distortion_accuracy,
-        severity_accuracy=severity_accuracy,
+            ConfusionMatrixDisplay.from_predictions(target, pred, labels=label_indices, display_labels=vocab, ax=ax)
+    scores = dict(
+        distortion_f1_macro = F1Score(average='macro')(preds[0], targets[0]).item(),
+        distortion_accuracy = accuracy(probs[0], targets[0]).item(),
+        severity_f1_macro = F1Score(average='macro')(preds[1], targets[1]).item(),
+        severity_accuracy = accuracy(probs[1], targets[1]).item(),
     )
+    return clf_report, scores
